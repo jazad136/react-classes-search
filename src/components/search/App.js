8d7556af8110fc9da0cd0363e2ab38b7c34d8dbb
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-07-24 18:43:49 -0400

    List out all first_name elements of Users array

--- src/components/search/App.js
+++ src/components/search/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './app.css'
+import {Users} from "./users"
+
 const App = () => {
   return (
     <div className="app">
-      <input type="text" placeholder="Search..." className="search" />
+      <input className="search" type="text" placeholder="Search..."  />
       <ul className="list"> 
-        <li className="listItem">John</li>
+        {/* <li className="listItem">John</li>
         <li className="listItem">Jane</li>
-        <li className="listItem">Mike</li>
+        <li className="listItem">Mike</li> */}
+        {Users.map((user) => ( 
+            <li className="listItem" key={user.id}>{user.first_name}</li>
+        ))}
       </ul>
     </div>
   )
